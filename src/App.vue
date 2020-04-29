fee9405789b61652e874220b2bf1b1d705c44cf5
123428653 <123428653@qq.com>
2020-04-29 18:29:35 +0800

    新增底部备案信息

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
     <router-view/>
+    <div class="footer">
+      <a href="http://www.beian.miit.gov.cn/" target="_blank">粤ICP备18069277号-1</a>
+    </div>
   </div>
 </template>
 
 <style>
 p,h2 {
   color: #dee2e6;
+}
+.footer {
+  text-align: center;
+  padding: 20px 0;
+}
+.footer a {
+  color: #fff;
 }
 </style>
